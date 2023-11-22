08f5162f53671e42bdf6ce378414864eb62fe692
restuprtma <restu.prtma05@gmail.com>
2023-11-22 12:36:44 +0700

    Membuat Fungsi dengan Nilai Kembalian

---
+++ jobsheet13/Ucapan09.java
@@ -0,0 +1,19 @@
+package jobsheet13;
+
+import java.util.Scanner;
+
+public class Ucapan09 {
+
+     public static String PenerimaUcapan() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Tuliskan NAMA orang yang ingin anda beri ucapan : ");
+        String namaOrang = sc.nextLine();
+        sc.close();
+        return namaOrang;
+     }
+
+     public static void main(String[] args) {
+        String nama = PenerimaUcapan();
+        System.out.println("Thank You "+nama+"\nMay the force be with you.");
+     }
+}
