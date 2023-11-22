b9221e3767c806a8a141edad5565e8282c7c8836
restuprtma <restu.prtma05@gmail.com>
2023-11-22 12:38:21 +0700

    Fungsi yang dapat meng-Calling Fungsi Lain

---
+++ jobsheet13/UcapanTerimaKasih09.java
@@ -0,0 +1,23 @@
+package jobsheet13;
+
+import java.util.Scanner;
+public class UcapanTerimaKasih09 {
+
+    public static String PenerimaUcapan() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Tuliskan NAMA orang yang ingin anda beri ucapan : ");
+        String namaOrang = sc.nextLine();
+        sc.close();
+        return namaOrang;
+    }
+
+    public static void UcapanTerimaKasih() {
+        String nama = PenerimaUcapan();
+        System.out.println("Thank You "+nama+" for being the best teacher in the world.\n"+
+        "You inspired in me a love for learning and made me feel like I cloud ask you anything.");
+    }
+    
+    public static void main(String[] args) {
+        UcapanTerimaKasih();
+    }
+}
