d5a0cb63a7024c78ad550defbe70c6cd4bd3119e
restuprtma <restu.prtma05@gmail.com>
2023-11-22 12:35:19 +0700

    Membuat Fungsi Tanpa Parameter

---
+++ jobsheet13/TerimaKasih09.java
@@ -0,0 +1,19 @@
+package jobsheet13;
+
+public class TerimaKasih09 {
+
+    public static void UcapanTerimaKasih(){
+        System.out.println("Thank you for being the best teacher in the world.\n"+
+        "You inspired in me a love for learning and made me feel like I cloud ask you anything.");
+    }
+    
+    public static void UcapanTambahan(String ucapan){
+        System.out.println(ucapan);
+    }
+
+    public static void main(String[] args) {
+        UcapanTerimaKasih();
+        String ucapan = "Terimakasih Pak... Bu... Semoga sehat selalu";
+        UcapanTambahan(ucapan);
+    }
+}
